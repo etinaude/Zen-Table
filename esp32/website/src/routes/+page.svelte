e12102e6798e87b25e41345a8df7946b51c9d70e
Etienne <eti@naude.dev>
2023-05-22 20:20:40 +1200

    svelte site

---
+++ esp32/website/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Pattern</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<h1>Pattern Picker</h1>
+
+<style lang="scss">
+</style>
---
+++ esp32/website/src/routes/lights/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import ColorPicker from 'svelte-awesome-color-picker';
+	let hex: string;
+</script>
+
+<h1>Colour Picker</h1>
+
+<div class="background"><div style="background: {hex}" /></div>
+
+<div class="picker">
+	<div>
+		<ColorPicker
+			bind:hex
+			isOpen={true}
+			isAlpha={false}
+			isInput={false}
+			isTextInput={false}
+			components={{
+				pickerWrapper: undefined
+			}}
+		/>
+	</div>
+</div>
+
+<div class="pattern">
+	<!-- TODO -->
+</div>
+
+<style lang="scss">
+	.background {
+		width: 100%;
+		height: 100%;
+		position: fixed;
+		left: 0;
+		top: 0;
+		background: white;
+		z-index: -1;
+
+		div {
+			width: 100%;
+			height: 100%;
+			opacity: 0.3;
+		}
+	}
+
+	.picker {
+		margin-top: 200px;
+		display: flex;
+		justify-content: center;
+	}
+</style>
